57b60e48679f9a6367890e7d869d2e845f9eb61b
Dev <deven2806@gmail.com>
2020-09-17 18:07:49 +0530

    Created db classes for storing data using Room

---
+++ app/src/main/java/com/example/tmdbclient/data/db/TVShowDao.kt
@@ -0,0 +1,20 @@
+package com.example.tmdbclient.data.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.tmdbclient.data.model.movie.Movie
+import com.example.tmdbclient.data.model.tvshow.TVShow
+
+@Dao
+interface TVShowDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveTVShows(tvShows: List<TVShow>)
+
+    @Query("DELETE FROM popular_tv_shows")
+    suspend fun deleteAllTVShows()
+
+    @Query("SELECT * FROM popular_tv_shows")
+    suspend fun getTVShows(tvShows: List<TVShow>)
+}
