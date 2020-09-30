5c6d7cabeac388325ad4b6afe736a7ac9a114db2
Dev <deven2806@gmail.com>
2020-09-30 12:46:03 +0530

    Added functionality to update popular movies RecyclerView from API.

--- app/src/main/java/com/example/tmdbclient/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/example/tmdbclient/presentation/movie/MovieActivity.kt
@@ -2,6 +2,9 @@ package com.example.tmdbclient.presentation.movie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
@@ -48,6 +51,36 @@ class MovieActivity : AppCompatActivity() {
             } else {
                 binding.movieProgressBar.visibility = View.GONE
                 Toast.makeText(applicationContext, "No data available", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.update,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_update -> {
+                updateMovies()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun updateMovies() {
+        binding.movieProgressBar.visibility = View.VISIBLE
+        val response = movieViewModel.updateMovieList()
+        response.observe(this, Observer {
+            if (it!= null) {
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility = View.GONE
+            } else {
+                binding.movieProgressBar.visibility = View.GONE
             }
         })
     }
